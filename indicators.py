e3774271f086d3eec3315807b52298d404924ed7
vnkotak <57126776+vnkotak@users.noreply.github.com>
2025-07-04 01:23:11 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -8,7 +8,9 @@ MACD_SIGNAL_DIFF = 1.0
 # === Common Keys ===
 SUPABASE_URL = "https://lfwgposvyckptsrjkkyx.supabase.co"  # e.g. "https://yourproject.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxmd2dwb3N2eWNrcHRzcmpra3l4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTg0MjI3MSwiZXhwIjoyMDY1NDE4MjcxfQ.7Pjsw_HpyE5RHHFshsRT3Ibpn1b6N4CO3F4rIw_GSvc"
-
+# === TELEGRAM SETUP ===
+TELEGRAM_BOT_TOKEN = "7468828306:AAG6uOChh0SFLZwfhnNMdljQLHTcdPcQTa4"
+TELEGRAM_CHAT_ID = "980258123"
 
 def calculate_rsi(series, period=14):
     delta = series.diff()
